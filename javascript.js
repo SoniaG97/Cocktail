51122d9a7fea95891ca84e4786004000bb20a104
Sonia Gordhandas <sgordhandas@hotmail.com>
2018-03-08 19:08:04 +0000

    final copy

---
+++ javascript.js
@@ -0,0 +1,19 @@
+$("#button").click(function(event) {
+  var text = $("#input").val();
+  $("#list").append('<li>' + text + '</li>');
+});
+
+.equal, .equal > div[class*='col-'] {
+         display: -webkit-box;
+         display: -moz-box;
+         display: -ms-flexbox;
+         display: -webkit-flex;
+         display: flex;
+         flex:1 0 auto;
+ }
+
+  width: 50%;
+    margin: 20px 25% 20px 25%;
+    text-align: center;
+    border-top: 1px solid #D8D8D8;
+    line-height: 0.1px;
